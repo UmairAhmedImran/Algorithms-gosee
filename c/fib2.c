be388b9b35d216e200e8ef8ed5e2beac8b7fcd01
umairahmedimran <umairahmedimranbutt@gmail.com>
2025-07-19 12:03:50 +0500

    corr: corrected and modified the code for memory managment

--- c/fib2.c
+++ c/fib2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int fib(int x);
+unsigned long long fib(int x);
 
 int main() {
-  int result = fib(2000);
-  printf("%d", result);
+  printf("%llu\n", fib(93));
   return 0;
 }
 
-int fib(int x) {
+unsigned long long fib(int x) {
   if (x == 0) return 0;
-  int arr[x];
+  unsigned long long * arr = malloc((x + 1) * sizeof(unsigned long long));
   arr[0] = 0, arr[1] = 1;
-  if (x > 2){
-    for (int i = 2; i <= x; i++) {
-      arr[i] = arr[i - 1] + arr[i - 2];
-    }
+  for (int i = 2; i <= x; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
   }
-  return arr[x];
+  unsigned long long result = arr[x];
+  free(arr);
+  return result;
 }
